64effaaa23f2f3e6a531dc3ae8fbd3d87eecd0db
Ignacio Monzón Acosta <ignaciomonzona@gmail.com>
2024-04-10 20:45:10 +0200

    Ejercicios resueltos

--- index.js
+++ index.js
@@ -12,8 +12,9 @@ const s5 = "and";
 
 // Print out the concatenated string
 
+const sTotal = `${s1} ${s2} ${s3} ${s4} ${s5} ${s1} ${s2} ${s3} ${s4}`;
 
-
+console.log (sTotal);
 
 /*******************************************
     Iteration 1.2 | Camel Tail
@@ -26,6 +27,15 @@ const part2 = "script";
 
 // Print the cameLtaiL-formatted string
 
+const part1Camel = part1.slice (0, 3) + part1[part1.length -1].toUpperCase(); // o tb const part2Camel = part1.slice (0, 3) + part1[3].toUpperCase(); 
+
+const part2Camel = part2.slice (0, 5) + part2[part2.length -1].toUpperCase(); //o tb const part2Camel = part2.slice (0, 3) + part2[5].toUpperCase();
+
+const result = `${part1Camel}${part2Camel}`;
+
+console. log (result);
+
+
 
 
 
@@ -36,10 +46,11 @@ const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
 
+const tipAmount = billTotal * 15 /100; 
 
 // Print out the tipAmount
 
-
+console.log(tipAmount); 
 
 
 /*******************************************
@@ -48,10 +59,11 @@ const billTotal = 84;
 
 // Generate a random integer between 1 and 10 (inclusive)
 
+let randomNumber = Math.floor(Math.random() * 10);      
 
 // Print the generated random number
 
-
+console.log(randomNumber); 
 
 /*******************************************
     Iteration 3.1 | Booleans
@@ -62,15 +74,29 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
+//false
+console.log (expression1);
 
 const expression2 = a || b;
+//true
+console.log (expression2);
 
 const expression3 = !a && b;
+//true
+console.log (expression3);
 
 const expression4 = !(a && b);
+//false
+console.log (expression4);
 
 const expression5 = !a || !b;
+//true
+console.log (expression5);
 
 const expression6 = !(a || b);
+//true
+console.log (expression6);
 
 const expression7 = a && a;
+//true
+console.log (expression7);
